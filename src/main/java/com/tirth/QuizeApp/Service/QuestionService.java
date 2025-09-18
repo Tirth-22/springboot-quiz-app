a73af050798d62ffc2c047df14fa9e10fd5a8296
Tirth-22 <tirthmakadia2006@gmail.com>
2025-09-19 00:27:05 +0530

    returns values to app

--- src/main/java/com/tirth/QuizeApp/Service/QuestionService.java
+++ src/main/java/com/tirth/QuizeApp/Service/QuestionService.java
@@ -1,4 +1,28 @@
 package com.tirth.QuizeApp.Service;
 
+import com.tirth.QuizeApp.Controller.Question;
+import com.tirth.QuizeApp.DAO.Questiondao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class QuestionService {
+
+    @Autowired
+    private Questiondao questiondao;
+
+    public List<Question> getAllQuestions() {
+        return questiondao.findAll();
+    }
+
+    public List<Question> getAllQuestionsByCategory(String category) {
+        return questiondao.findByCategory(category);
+    }
+
+    public String addQuestion(Question question) {
+        questiondao.save(question);
+        return "Success";
+    }
 }
