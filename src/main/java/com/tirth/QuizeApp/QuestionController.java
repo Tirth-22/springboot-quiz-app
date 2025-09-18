130eebb696ec8d5803cfd7a59bf559c141d6a8a0
Tirth-22 <tirthmakadia2006@gmail.com>
2025-09-18 12:10:14 +0530

    return value to server

--- src/main/java/com/tirth/QuizeApp/QuestionController.java
+++ src/main/java/com/tirth/QuizeApp/QuestionController.java
@@ -1,4 +1,14 @@
 package com.tirth.QuizeApp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
 public class QuestionController {
+    @GetMapping("AllQuestions")
+    public String getQuestions(){
+        return "Tirth spring boot test";
+    }
 }
