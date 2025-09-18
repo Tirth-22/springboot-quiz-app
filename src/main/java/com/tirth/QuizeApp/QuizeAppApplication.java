7651f9cd2e92aa0916818d2edeee9e09f947218d
Tirth-22 <tirthmakadia2006@gmail.com>
2025-09-18 12:09:25 +0530

    set request from server

--- src/main/java/com/tirth/QuizeApp/QuizeAppApplication.java
+++ src/main/java/com/tirth/QuizeApp/QuizeAppApplication.java
@@ -2,6 +2,9 @@ package com.tirth.QuizeApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class QuizeAppApplication {
@@ -10,4 +13,13 @@ public class QuizeAppApplication {
 		SpringApplication.run(QuizeAppApplication.class, args);
 	}
 
+    @RestController
+    @RequestMapping("question")
+    public static class QuestionController {
+        @GetMapping("allQuestion")
+
+        public String getAllQuestions(){
+            return "this is tirth starting of springboot";
+        }
+    }
 }
