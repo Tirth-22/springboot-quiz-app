a235298e06bda40ef466168baa1d1424a97eef26
Tirth-22 <tirthmakadia2006@gmail.com>
2025-09-20 00:30:04 +0530

    db data interface

--- src/main/java/com/tirth/QuizeApp/DAO/Questiondao.java
+++ src/main/java/com/tirth/QuizeApp/DAO/Questiondao.java
@@ -1,4 +1,13 @@
-package com.tirth.QuizeApp;
+package com.tirth.QuizeApp.DAO;
 
-public class Questiondao {
+import com.tirth.QuizeApp.Controller.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface Questiondao extends JpaRepository<Question,Integer> {
+
+    List<Question> findByCategory(String category);
 }
