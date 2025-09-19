a773e1073d6dcc1b9fef87ea936b94b086c3dc4e
Tirth-22 <tirthmakadia2006@gmail.com>
2025-09-20 00:16:50 +0530

    SpringBoot run file

--- src/main/java/com/tirth/QuizeApp/QuizeAppApplication.java
+++ src/main/java/com/tirth/QuizeApp/QuizeAppApplication.java
@@ -2,24 +2,10 @@ package com.tirth.QuizeApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class QuizeAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(QuizeAppApplication.class, args);
-	}
-
-    @RestController
-    @RequestMapping("question")
-    public static class QuestionController {
-        @GetMapping("allQuestion")
-
-        public String getAllQuestions(){
-            return "this is tirth starting of springboot";
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(QuizeAppApplication.class, args);
     }
 }
